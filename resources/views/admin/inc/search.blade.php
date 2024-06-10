a05c537cab0f4cb0e691c2d632365a7c8e838453
DhruvSvt <dhruvsvt08@gmail.com>
2024-06-10 11:17:38 +0530

    Update search.blade.php

--- resources/views/admin/inc/search.blade.php
+++ resources/views/admin/inc/search.blade.php
@@ -12,15 +12,17 @@
                 <div class="col-md-1 mt-3 mt-md-0">
                     <button type="submit" class="btn btn-gray-800 btn-block">Search</button>
                 </div>
-                <div class="col-md-2 mt-3 mt-md-0">
-                    <select class="form-control" name="rows" onchange="this.form.submit()">
-                        <option value="25" {{ $row == '25' ? 'selected' : '' }}>25</option>
-                        <option value="50" {{ $row == '50' ? 'selected' : '' }}>50</option>
-                        <option value="100" {{ $row == '100' ? 'selected' : '' }}>100</option>
-                        <option value="250" {{ $row == '250' ? 'selected' : '' }}>250</option>
-                        <option value="500" {{ $row == '500' ? 'selected' : '' }}>500</option>
-                        <option value="all" {{ $row == 'all' ? 'selected' : '' }}>All</option>
-                    </select> entries per page
+                <div class="col-md-7 mt-3 mt-md-0">
+                    <div style="max-width: 100px;float: right;">
+                        <select class="form-control text-center" name="rows" onchange="this.form.submit()">
+                            <option value="25" {{ $row == '25' ? 'selected' : '' }}>25</option>
+                            <option value="50" {{ $row == '50' ? 'selected' : '' }}>50</option>
+                            <option value="100" {{ $row == '100' ? 'selected' : '' }}>100</option>
+                            <option value="250" {{ $row == '250' ? 'selected' : '' }}>250</option>
+                            <option value="500" {{ $row == '500' ? 'selected' : '' }}>500</option>
+                            <option value="all" {{ $row == 'all' ? 'selected' : '' }}>All</option>
+                        </select> entries per page
+                    </div>
                 </div>
             </div>
         </form>
